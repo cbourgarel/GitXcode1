8025527d588e20cbdcaf8880602b32a08fd45b33
cbourgarel <32009959+cbourgarel@users.noreply.github.com>
2017-09-16 10:16:58 +0200

    Initial Commit

---
+++ GitXcode1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXcode1
+//
+//  Created by Christophe Bourgarel on 16/09/2017.
+//  Copyright © 2017 Christophe Bourgarel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
